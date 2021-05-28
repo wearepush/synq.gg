2308245e42e15cc70c1591105b5be694eaafbeea
Iuriy-Budnikov <iuriy.budnikov@gmail.com>
2021-05-28 18:16:01 +0300

    update images

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,12 +1,12 @@
-import React from "react";
-import "./section.scss";
+import React from 'react';
+import './section.scss';
 
-import MainImageWebp from "../../images/center-image-item-2x.webp";
-import MainImagePng from "../../images/center-image-item-2x.png";
-import LeftImageWebp from "../../images/left-image-item-2x.webp";
-import LeftImagePng from "../../images/left-image-item-2x.png";
-import RightImageWebp from "../../images/right-image-item-2x.webp";
-import RightImagePng from "../../images/right-image-item-2x.png";
+import MainImageWebp from '../../images/center-image-item-2x.webp';
+import MainImagePng from '../../images/center-image-item-2x.png';
+import LeftImageWebp from '../../images/left-image-item-2x.webp';
+import LeftImagePng from '../../images/left-image-item-2x.png';
+import RightImageWebp from '../../images/right-image-item-2x.webp';
+import RightImagePng from '../../images/right-image-item-2x.png';
 
 const Image = ({ alt, webp, png }) => {
   return (
@@ -23,15 +23,11 @@ const Section = () => (
       <div className="section__content">
         <div className="section__pre-title">Bet Together!</div>
         <div className="section__title">
-          Wants <span>to be the first</span>? Subscribe to the newsletter and be
-          aware of all the most favorable sport odds.
+          Wants <span>to be the first</span>? Subscribe to the newsletter and be aware of all the
+          most favorable sport odds.
         </div>
         <form className="section__form">
-          <input
-            type="email"
-            className="section__form__input"
-            placeholder="Enter your e-mail"
-          />
+          <input type="email" className="section__form__input" placeholder="Enter your e-mail" />
           <button className="section__form__button" type="submit">
             Subscribe
           </button>
@@ -46,7 +42,7 @@ const Section = () => (
         <Image png={MainImagePng} webp={MainImageWebp} alt="synq 2" />
       </div>
       <div className="section__image-item section__image-item--right">
-        <Image png={RightImageWebp} webp={RightImagePng} alt="synq 3" />
+        <Image png={RightImagePng} webp={RightImageWebp} alt="synq 3" />
       </div>
     </div>
   </div>
