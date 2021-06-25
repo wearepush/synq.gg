ee180bcc5bcfae47b2231c14e45bb8bd29231e5d
Dmitriy Saltovskiy <dima.s@wearepush.co>
2021-06-25 21:07:44 +0300

    Updates

---
+++ component---src-pages-index-js-6c738938536ce8db6e9d.js
@@ -0,0 +1,2 @@
+(self.webpackChunksynq=self.webpackChunksynq||[]).push([[678],{7704:function(n,e,u){"use strict";u.r(e);var t=u(7294),f=u(5444);e.default=function(){return(0,t.useEffect)((function(){(0,f.navigate)("/intheknow")}),[]),null}}}]);
+//# sourceMappingURL=component---src-pages-index-js-6c738938536ce8db6e9d.js.map
