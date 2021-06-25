f23917e1f52824f7e0c347a443b13da944e86f8b
Dmitriy Saltovskiy <dima.s@wearepush.co>
2021-06-25 22:45:37 +0300

    fix/typos

--- src/data.js
+++ src/data.js
@@ -4,22 +4,22 @@ const data = {
   intheknow: {
     headerTitle: <>Live sportsbetting takes</>,
     bodyTitle: <>Be in the know.</>,
-    bodyDescription: <>Don’t get left behind. Stay up to date with the latest sportsbettingodds, picks, and takes from punters like you. <span>You in?</span></>,
+    bodyDescription: <>Don’t get left behind. Stay up to date with the latest sportsbetting odds, picks, and takes from punters like you. <span>You in?</span></>,
   },
   strongertogether: {
     headerTitle: <>Live sportsbetting takes</>,
-    bodyTitle: <>We’re stronger together</>,
+    bodyTitle: <>We’re stronger together.</>,
     bodyDescription: <>Knowledge is power – share it. Join our community of punters to stay up to date with the latest sportsbetting odds, picks, and takes. <span>You in?</span></>,
   },
   bettogether: {
     headerTitle: <>Live sportsbetting takes</>,
     bodyTitle: <>Bet together.</>,
-    bodyDescription: <>We’re better together. Share your ideas and learn from others likeyou. Join our sportsbetting community for the latest sportsbetting odds, picks, and takes. <span>You in?</span></>,
+    bodyDescription: <>We’re better together. Share your ideas and learn from others like you. Join our sportsbetting community for the latest sportsbetting odds, picks, and takes. <span>You in?</span></>,
   },
   edge: {
     headerTitle: <>Live sportsbetting takes</>,
-    bodyTitle: <>Get an edge</>,
-    bodyDescription: <>Don’t get left behind. Stay up to date with the latest sportsbettingodds, picks, and takes from punters like you. <span>You in?</span></>,
+    bodyTitle: <>Get an edge.</>,
+    bodyDescription: <>Don’t get left behind. Stay up to date with the latest sportsbetting odds, picks, and takes from punters like you. <span>You in?</span></>,
   },
   home: {
     headerTitle: <>Your new sportsbetting home</>,
@@ -33,7 +33,7 @@ const data = {
   },
   index: {
     headerTitle: <>Sportsbetting Takes. Always live, always free.</>,
-    bodyTitle: <>Be in the know</>,
+    bodyTitle: <>Be in the know.</>,
     bodyDescription: <>Join our community of punters <span>to be the first</span> to get exclusive access to the best sports betting odds, picks, and takes. You in?</>,
   }
 };
